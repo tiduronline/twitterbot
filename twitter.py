1eacdf8425a1570b200921d82b475d96369cae80
tiduronline <didesignin@gmail.com>
2013-07-28 01:35:04 +0700

    revise for BBM emot char

--- twitter.py
+++ twitter.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+'''
+Twitter Bot Retweet version
+Copyright 2013, Verri Andriawan, didesignin.com 
+
+http://didesignin.com/
+'''
 
 from time import sleep
-from sys import exit
+from sys import exit, path
 from datetime import datetime
-import tweepy
+import tweepy	
 
 # Setting API
 CONSUMER_KEY = '-'
 CONSUMER_SECRET = '-'
 ACCESS_KEY = '-'
 ACCESS_SECRET = '-'
+USERNAME = '-'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -21,7 +30,7 @@ old_stack = []
 try:
 	while True:
 		# GET 20 Mentions
-		ment = api.mentions()
+		ment = api.mentions_timeline()
 		new_stack = []
 
 		if(len(old_stack) > 0) :
@@ -30,7 +39,7 @@ try:
 				if(stat.id in old_stack):
 					break
 				else:
-					if(stat.user.screen_name!='id_twitter'):
+					if(stat.user.screen_name != USERNAME):
 						api.retweet(stat.id)
 						print 'retweet : '+stat.text+' from '+stat.user.screen_name
 					sleep(5)
@@ -39,7 +48,7 @@ try:
 		else : 
 			for stat in ment:
 				new_stack.append(stat.id)
-				if(stat.user.screen_name!='id_twitter'):
+				if(stat.user.screen_name != USERNAME):
 					api.retweet(stat.id)
 				sleep(5)
 			old_stack = new_stack
